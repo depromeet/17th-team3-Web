df6a6e0542e1f8a1c09a77f0284f94872a373af9
Mark Kwon <redhero8830@gmail.com>
2025-09-07 02:20:25 +0900

    [Feat] 설문 퍼널 아이템 컴포넌트 구현 (#12)

---
+++ src/app/surveyitem/page.tsx
@@ -0,0 +1,13 @@
+import SurveyItem from '@/app/survey/_components/SurveyItem';
+
+const page = () => {
+  return (
+    <div className="flex h-[100%] flex-col gap-4 bg-gray-100 p-4">
+      <SurveyItem>술안주랑 함께, 한잔하는 자리가 좋아요</SurveyItem>
+      <SurveyItem selected>해장 제대로! 얼큰한 게 좋아요</SurveyItem>
+      <SurveyItem>어떤 분위기든 좋아요!</SurveyItem>
+    </div>
+  );
+};
+
+export default page;
