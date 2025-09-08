54b0b92938ee903426a148567be450d561f99f7d
Mark Kwon <redhero8830@gmail.com>
2025-09-09 02:06:48 +0900

    [Refactor]! 사용성 향상을 위해 Title 컴포넌트 props명 변경 (#10) - children에서 description으로 변경

--- src/app/_components/ui/Title.tsx
+++ src/app/_components/ui/Title.tsx
@@ -1,14 +1,14 @@
 interface TitleProps {
   title: string;
-  children?: React.ReactNode;
+  description?: React.ReactNode;
 }
 
-const Title = ({ title, children }: TitleProps) => {
+const Title = ({ title, description }: TitleProps) => {
   return (
     <header>
       {/* todo: 스타일 점검 */}
       <h1 className="text-2xl/[1.5] font-bold whitespace-pre-wrap">{title}</h1>
-      {children && <p className="mt-2 text-sm/[1.7] text-gray-800">{children}</p>}
+      {description && <p className="mt-2 text-sm/[1.7] text-gray-800">{description}</p>}
     </header>
   );
 };
