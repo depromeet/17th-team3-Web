5595739853a87afb4280037086d0ac3514016247
rhctmxk <rhctmxk@naver.com>
2025-09-09 00:04:08 +0900

    [Setting] Server Action 세팅 - API 호출

---
+++ src/app/_services/surveyAction.ts
@@ -0,0 +1,9 @@
+import type { SurveyResult } from '@/app/survey/_models/types';
+
+const saveSurveyResult = (input: SurveyResult) => {
+  // TODO: DB/외부 API 저장
+  // await fetch(...);
+  return { ok: true };
+};
+
+export default saveSurveyResult;
