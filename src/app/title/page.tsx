144fc7b6d0ebfc0ae411fc8e401a20ceaa7bf2cc
Mark Kwon <redhero8830@gmail.com>
2025-09-12 04:11:35 +0900

    [Feat] TitleGroup 컴포넌트 구현 (#10)

--- src/app/title/page.tsx
+++ src/app/title/page.tsx
@@ -1,4 +1,5 @@
 import { Heading, Text } from '@/app/_components/typography';
+import TitleGroup from '@/app/_components/typography/TitleGroup';
 
 const Page = () => {
   return (
@@ -19,6 +20,14 @@ const Page = () => {
       <br />
       <Heading as="h3">당신의 이름을 알려주세요</Heading>
       <Text>원하는 별명을 입력해도 괜찮아요.</Text>
+      <br />
+      <TitleGroup title="회원가입" description="정보를 입력해주세요" />
+      <TitleGroup
+        title={`환영합니다!\n서비스를 시작해보세요`}
+        description="몇 가지 정보만 입력하면 됩니다"
+      />
+      <TitleGroup title="설정" description="계정 정보를 관리하세요" align="left" />
+      <TitleGroup title="마지막 단계" level="h3" description="거의 다 끝났어요!" />
     </div>
   );
 };
