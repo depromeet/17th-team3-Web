c928824954003bf087d9149586e65b9ffaeb9d85
Mark Kwon <redhero8830@gmail.com>
2025-09-14 04:11:59 +0900

    [Feat] OAuth code 추출 커스텀훅 useAuthParams 구현 (#26)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,17 @@
 
 import LoginButton from '@/app/login/_components/LoginButton';
 
+// const OAUTH_URL = process.env.NEXT_PUBLIC_OAUTH_URL!;
+const OAUTH_URL = 'https://www.naver.com';
+
 const HomePage = () => {
+  const handleLogin = () => {
+    window.location.href = OAUTH_URL;
+  };
+
   return (
     <main className="mx-auto flex w-full flex-col items-center justify-center p-4">
-      <LoginButton onLogin={() => console.log('로그인!')} />
+      <LoginButton onLogin={handleLogin} />
     </main>
   );
 };
