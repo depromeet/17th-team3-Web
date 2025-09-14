fb9d0791e5a51aa122b228170a597d27423f1d90
jinhyuek <jjh943202@naver.com>
2025-09-15 00:52:05 +0900

    [Refactor] 버튼 제출 타입 제거 및 클릭 이벤트 유지

--- src/app/any/buttonTest/page.tsx
+++ src/app/any/buttonTest/page.tsx
@@ -14,9 +14,7 @@ const ButtonTestPage = () => {
       <Button theme="gray">회색 버튼</Button>
 
       {/* HTML 속성 사용 */}
-      <Button type="submit" onClick={() => alert('클릭됨!')}>
-        제출
-      </Button>
+      <Button onClick={() => alert('클릭됨!')}>제출</Button>
 
       {/* 커스텀 스타일 */}
       <Button className="max-w-32 bg-blue-500 font-bold text-white shadow-lg active:bg-blue-600">
