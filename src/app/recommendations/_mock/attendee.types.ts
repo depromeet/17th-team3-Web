1992b82c8dd707f3ed14f85404fddbbfb59f2927
jinhyuek <jjh943202@naver.com>
2025-09-20 23:07:27 +0900

    [Feat]! 추천 페이지 타입 정의 및 목데이터 추가
    참석자 데이터 구조를 위한 타입과 목데이터를 별도 폴더로 분리하여 관리
    
    - attendee.types.ts: Attendee, AttendeesData 인터페이스 정의
    - attendee.mock.ts: 테스트용 참석자 목데이터 추가
    - JSDoc으로 상세 문서화 완료

---
+++ src/app/recommendations/_mock/attendee.types.ts
@@ -0,0 +1,29 @@
+import { FoodKey } from '@/app/_constants/Menu';
+
+/**
+ * 참석자 개인 정보 및 음식 선호도를 나타내는 타입
+ */
+export interface Attendee {
+  /** 참석자 고유 식별자 */
+  id: string;
+  /** 참석자 이름 */
+  name: string;
+  /** 참석자 아바타 이모지 */
+  avatar: string;
+  /** 선호하는 음식 카테고리 목록 */
+  preferredFoods: FoodKey[];
+  /** 먹지 못하는 음식 카테고리 목록 */
+  avoidedFoods: FoodKey[];
+}
+
+/**
+ * 참석자 데이터 집합을 나타내는 타입
+ */
+export interface AttendeesData {
+  /** 전체 참석자 수 */
+  totalAttendees: number;
+  /** 아직 선택하지 않은 참석자 수 */
+  notYetSelectedAttendees: number;
+  /** 참석자 목록 */
+  attendees: Attendee[];
+}
