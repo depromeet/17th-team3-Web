e94d2736c2a1a79c28164a43fe029366c7cbd825
Mark Kwon <redhero8830@gmail.com>
2025-09-21 17:30:38 +0900

    refactor: 폼 작성 컴포넌트 FormStep 컴포넌트로 분리 (#41)

--- src/app/meetings/create/page.tsx
+++ src/app/meetings/create/page.tsx
@@ -9,35 +9,68 @@ import Input from '@/app/_components/ui/Input';
 const CreatePage = () => {
   const [name, setName] = useState('');
 
+  const handleCancel = () => {};
+  const handleNext = () => {};
+
   return (
     <div className="flex h-[100dvh] flex-col bg-[url('/images/mobileBackground.svg')] bg-cover">
       <TopNavigation title="모임 만들기" showBackButton />
       {/* todo: StepIndicator 컴포넌트로 추가 */}
-      <main className="flex flex-1 flex-col">
-        <div className="px-4 pt-8 pb-4">
-          <h1 className="heading-3 font-bold">모임 이름은 무엇인가요?</h1>
-        </div>
-
-        <div className="flex-1 px-4">
-          <Input
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            onClear={() => setName('')}
-            placeholder="디프만 3팀"
-            showClearButton
-          />
-        </div>
-
-        <div className="flex gap-3 px-5 pt-3 pb-6">
-          <Button theme="gray" className="w-[114px]">
-            취소
-          </Button>
-          <Button status={name ? 'normal' : 'disabled'} className="flex-1">
-            다음 단계로
-          </Button>
-        </div>
-      </main>
+      <FormStep
+        title="모임 이름은 무엇인가요?"
+        onNext={handleNext}
+        onCancel={handleCancel}
+        isNextDisabled={!name} // todo: isNextDisabled 유효성 검증에 따른 disable 추가
+        prevButtonText="취소"
+      >
+        <Input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          onClear={() => setName('')}
+          placeholder="디프만 3팀"
+          showClearButton
+        />
+      </FormStep>
     </div>
+  );
+};
+
+interface FormStepProps {
+  title: string;
+  children: React.ReactNode;
+  onNext: () => void;
+  onCancel: () => void;
+  isNextDisabled?: boolean;
+  prevButtonText?: string;
+  nextButtonText?: string;
+}
+
+const FormStep = ({
+  title,
+  children,
+  onNext,
+  onCancel,
+  isNextDisabled,
+  prevButtonText = '이전',
+  nextButtonText = '다음 단계로',
+}: FormStepProps) => {
+  return (
+    <main className="flex flex-1 flex-col">
+      <div className="px-4 pt-8 pb-4">
+        <h1 className="heading-3 font-bold">{title}</h1>
+      </div>
+
+      <div className="flex-1 px-4">{children}</div>
+
+      <div className="flex gap-3 px-5 pt-3 pb-6">
+        <Button theme="gray" onClick={onCancel} className="w-[114px]">
+          {prevButtonText}
+        </Button>
+        <Button status={isNextDisabled ? 'disabled' : 'normal'} onClick={onNext} className="flex-1">
+          {nextButtonText}
+        </Button>
+      </div>
+    </main>
   );
 };
 
