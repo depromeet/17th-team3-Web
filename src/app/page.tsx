d8450581ba667261d6078be315b67a64494dd467
Mark Kwon <redhero8830@gmail.com>
2025-09-21 22:55:48 +0900

    [Design] 홈페이지 화면 배경 디자인 시스템 적용 (#41)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,24 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { Heading, Text } from '@/app/_components/typography';
 import Button from '@/app/_components/ui/Button';
 
 const HomePage = () => {
   return (
-    <div className="flex h-[100dvh] flex-col bg-[url('/images/mobileBackground.svg')] bg-cover">
-      <div className="px-8 pt-16 pb-3">
+    <div className="flex h-[100dvh] flex-col background-2">
+      <div className="flex flex-col gap-2 px-8 pt-16 pb-3">
         <Image src={'/images/mumozzi-home-sm.svg'} alt="모무찌 작은 로고" width={36} height={36} />
-        <h1 className="mt-2 bg-gradient-to-r from-orange-900 to-orange-500 bg-clip-text heading-2 font-bold text-transparent">
-          우리 어디서 모무찌?
-        </h1>
+        <Heading level="h2">우리 어디서 모무찌?</Heading>
       </div>
-      <div className="flex flex-1 flex-col items-center justify-center gap-3">
+      <div className="flex flex-1 flex-col items-center justify-center gap-4">
         <Image
           src={'/images/mumozzi-home.svg'}
           alt="모무찌 홈 대표 로고"
           width={101}
           height={101}
         />
-        <p className="mt-4 body-3 font-medium text-neutral-1400">
-          아직 진행 중인 식당 추천이 없어요
-        </p>
+        <Text className="text-neutral-1400">아직 진행 중인 식당 추천이 없어요</Text>
       </div>
       <div className="px-5 pt-3 pb-6">
         <Button>
