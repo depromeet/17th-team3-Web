511535125695b96c75ec2d6930c5c441308f2cba
Mark Kwon <redhero8830@gmail.com>
2025-09-23 01:11:01 +0900

    [Design] 로그인 페이지 배경 스타일 적용 (#26)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,9 +4,9 @@ import LoginButton from '@/app/login/_components/LoginButton';
 
 const LoginPage = () => {
   return (
-    <main className="mx-auto flex w-full flex-col items-center justify-center p-4">
+    <div className="flex h-[100dvh] w-full flex-col items-center justify-center p-4 background-3">
       <LoginButton />
-    </main>
+    </div>
   );
 };
 
