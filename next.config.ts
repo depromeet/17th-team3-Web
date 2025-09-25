72b41d5fb0cbbbd635b85d8c986208962c06ca4a
jinhyuek <jjh943202@naver.com>
2025-09-26 00:57:28 +0900

    [Feat] mocking 이미지 출처 허용위한 nextConfg 설정

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,17 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /** Next 빌드 아웃풋 최적화 (standalone) */
   output: 'standalone',
-  // (옵션) 이미지 외부 도메인 쓰면 domains 등록 필요
-  // images: { remotePatterns: [{ protocol: 'https', hostname: '...'}] },
+  // Unsplash 이미지 사용을 위한 도메인 설정
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
