f727dd44dcd3006285978b8426d7c5569ad56dce
Mark Kwon <redhero8830@gmail.com>
2025-10-07 00:40:59 +0900

    [Chore] 카카오 리다이렉트 주소 상수로 분리 (#56)

--- src/app/login/_components/LoginButton.tsx
+++ src/app/login/_components/LoginButton.tsx
@@ -4,15 +4,8 @@ import Image from 'next/image';
 
 import { cn } from '@/app/_lib/cn';
 
-const REDIRECT_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'http://localhost:3000/auth/callback'
-    : 'https://www.momuzzi.site/auth/callback';
+const REDIRECT_URL = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URL!;
 const CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
-
-interface LoginButtonProps {
-  provider?: 'kakao';
-}
 
 const PROVIDER_CONFIG = {
   kakao: {
@@ -22,6 +15,10 @@ const PROVIDER_CONFIG = {
     redirectUrl: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URL)}`,
   },
 };
+
+interface LoginButtonProps {
+  provider?: 'kakao';
+}
 
 const LoginButton = ({ provider = 'kakao' }: LoginButtonProps) => {
   const config = PROVIDER_CONFIG[provider];
