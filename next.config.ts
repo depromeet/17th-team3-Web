5b1cf0157109315675fbc924d85932cab8888531
Mark Kwon <redhero8830@gmail.com>
2025-10-10 02:03:18 +0900

    [Chore] next.confing.ts 불필요한 proxy 설정 제거 (#56)

--- next.config.ts
+++ next.config.ts
@@ -17,14 +17,6 @@ const nextConfig: NextConfig = {
 
   // (옵션) 이미지 외부 도메인 쓰면 domains 등록 필요
   // images: { remotePatterns: [{ protocol: 'https', hostname: '...'}] },
-  async rewrites() {
-    return [
-      {
-        source: '/api/proxy/:path*',
-        destination: 'https://api.momuzzi.site/api/v1/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
