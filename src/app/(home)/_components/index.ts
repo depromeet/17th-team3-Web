75910bb791b4285f9d66b388361f1949b6fafe5c
Mark Kwon <redhero8830@gmail.com>
2025-10-19 16:31:45 +0900

    [Refactor] SectionHeader 컴포넌트 분리 (#60)

--- src/app/(home)/_components/index.ts
+++ src/app/(home)/_components/index.ts
@@ -1,3 +1,4 @@
-export { default as EndedMeetingCard } from './EndedMeetingCard';
-export { default as EmptyActiveMeetingCard } from './EmptyActiveMeetingCard';
 export { default as ActiveMeetingCard } from './ActiveMeetingCard';
+export { default as EmptyActiveMeetingCard } from './EmptyActiveMeetingCard';
+export { default as EndedMeetingCard } from './EndedMeetingCard';
+export { default as SectionHeader } from './SectionHeader';
