57e011f83788a330ff37e4bd5d27eaa4f4db61b4
jinhyuek <jjh943202@naver.com>
2025-10-21 23:57:01 +0900

    [Feat] 분석 페이지 및 관련 컴포넌트 추가 - 분석 페이지를 위한 AnalysisLayout 및 AnalysisPage 컴포넌트를 구현했습니다. - 레스토랑 정보를 표시하는 RestaurantCard 및 RestaurantCardSwiper 컴포넌트를 추가했습니다. - PieChart 컴포넌트와 관련 차트 설정을 포함하여 데이터 시각화를 지원합니다.

---
+++ src/app/events/[eventId]/analysis/_components/RestaurantCardSwiper.tsx
@@ -0,0 +1,96 @@
+'use client';
+import { useEffect, useRef, useState } from 'react';
+
+import useEmblaCarousel from 'embla-carousel-react';
+import { ListIcon } from 'lucide-react';
+import Link from 'next/link';
+import { useParams } from 'next/navigation';
+import { parseAsInteger, useQueryState } from 'nuqs';
+
+import { cn } from '@/app/_lib/cn';
+import { RecommendedPlace } from '@/app/_services/places';
+import RestaurantCard from '@/app/events/[eventId]/analysis/_components/RestaurantCard';
+
+interface RestaurantCardSwiperProps {
+  places: RecommendedPlace[];
+}
+const RestaurantCardSwiper = ({ places }: RestaurantCardSwiperProps) => {
+  const params = useParams();
+  const { eventId } = params;
+
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+  const [picks] = useQueryState('picks', parseAsInteger.withDefault(5));
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const prevPicksRef = useRef(picks);
+
+  const totalPicks = Math.min(picks, places.length);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    const onSelect = () => {
+      setSelectedIndex(emblaApi.selectedScrollSnap());
+    };
+
+    emblaApi.on('select', onSelect);
+    onSelect();
+
+    return () => {
+      emblaApi.off('select', onSelect);
+    };
+  }, [emblaApi]);
+
+  useEffect(() => {
+    const prevPicks = prevPicksRef.current;
+    if (!emblaApi) return;
+
+    if (picks > prevPicks) {
+      setTimeout(() => {
+        emblaApi.scrollTo(prevPicks);
+      }, 500);
+    }
+
+    prevPicksRef.current = picks;
+  }, [emblaApi, picks]);
+
+  return (
+    <div className="flex w-full flex-col gap-5 pt-4 pb-10">
+      <div className="flex w-full items-center justify-between px-5">
+        <div className="flex gap-2 body-3 font-semibold">
+          <span className="text-white">{`${selectedIndex ? selectedIndex + 1 : 1}`}</span>
+          <span className="text-neutral-600">/</span>
+          <span className="text-neutral-600">{totalPicks}</span>
+        </div>
+        <Link
+          href={`/events/${eventId}/restaurants?picks=${picks}`}
+          replace={true}
+          prefetch={true}
+          className="flex h-8 items-center gap-1 rounded-full border border-white-alpha-3 bg-white-alpha-2 pr-3 pl-2"
+        >
+          <ListIcon className="h-4 w-4 text-white" />
+          <span className="label-2 font-semibold text-white">리스트 보기</span>
+        </Link>
+      </div>
+
+      <div className="overflow-hidden" ref={emblaRef}>
+        <div className="pan-y flex w-full">
+          {places.slice(0, picks).map((place, index) => (
+            <div
+              key={index}
+              className={cn(
+                'min ml-1.5 h-[131.7vw] max-h-[623px] flex-[0_0_85%] [transform:translate3d(0,0,0)] overflow-hidden rounded-2xl',
+                index === 0 && 'ml-5',
+                index === places.length - 1 && 'mr-5'
+              )}
+            >
+              <RestaurantCard place={place} index={index} activeIndex={selectedIndex} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantCardSwiper;
