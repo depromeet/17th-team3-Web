e219923889822ee0a0bf62e4517fafcdafb64e6b
rhctmxk <annawlgus5@gmail.com>
2025-10-30 08:19:26 +0900

    [Design] 임시 로딩 이미지 -> gif 변경

--- src/app/survey/_components/ui/LoadingOverlay.tsx
+++ src/app/survey/_components/ui/LoadingOverlay.tsx
@@ -1,16 +1,12 @@
 'use client';
 
-import Image from 'next/image';
-
 const LoadingOverlay = () => {
   return (
     <div className="fixed inset-0 z-[999] flex items-center justify-center bg-black/60">
-      <Image
-        src="/images/momuzzi-survey-loading.svg"
+      <img
+        src="/images/momuzzi-survey-loading.gif"
         alt="설문 저장 중"
-        width={120}
-        height={200}
-        priority
+        className="h-auto w-[270px]"
       />
     </div>
   );
