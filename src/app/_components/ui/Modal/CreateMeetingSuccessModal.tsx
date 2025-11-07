9ab369c73da275df68674e23445dae0779c9f706
Mark Kwon <redhero8830@gmail.com>
2025-11-08 00:00:17 +0900

    [Feat] 모임생성 성공 모달 CreateMeetingSuccessModal 컴포넌트 구현 (#76)

---
+++ src/app/_components/ui/Modal/CreateMeetingSuccessModal.tsx
@@ -0,0 +1,39 @@
+import { Copy } from 'lucide-react';
+import Image from 'next/image';
+
+import BaseModal from '@/app/_components/ui/Modal/BaseModal';
+
+interface CreateMeetingSuccessModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const CreateMeetingSuccessModal = ({ isOpen, onClose }: CreateMeetingSuccessModalProps) => {
+  return (
+    <BaseModal isOpen={isOpen} onClose={onClose}>
+      <div className="flex flex-col items-center pb-13 select-none">
+        <Image src="/images/confetti.svg" alt="폭죽 아이콘" width={100} height={100} />
+        <div className="flex flex-col items-center gap-2 pt-1 pb-2">
+          <h3 className="title-gradient heading-3 font-bold">모임이 생성되었어요!</h3>
+          <p className="text-center body-3 whitespace-pre-wrap text-neutral-800">
+            {`더 많은 멤버가 취향 설문에 참여하도록\n지금 링크를 공유해보세요!`}
+          </p>
+        </div>
+        <div className="flex gap-4">
+          <div className="flex flex-col gap-3 px-4 py-3">
+            <Image src="/icons/kakaotalk.svg" alt="카카오톡 아이콘" width={52} height={52} />
+            <div className="label-1 text-neutral-1000">카카오톡</div>
+          </div>
+          <div className="flex flex-col gap-3 px-4 py-3">
+            <div className="flex h-13 w-13 items-center justify-center rounded-full bg-neutral-200">
+              <Copy size={27} className="text-neutral-1300" />
+            </div>
+            <div className="label-1 text-neutral-1000">URL 복사</div>
+          </div>
+        </div>
+      </div>
+    </BaseModal>
+  );
+};
+
+export default CreateMeetingSuccessModal;
--- src/app/events/[eventId]/overview/_components/OverviewClientPage.tsx
+++ src/app/events/[eventId]/overview/_components/OverviewClientPage.tsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 
 import { useParams, useRouter, useSearchParams } from 'next/navigation';
 
+import CreateMeetingSuccessModal from '@/app/_components/ui/Modal/CreateMeetingSuccessModal';
+import { useDisclosure } from '@/app/_hooks/useDisclosure';
 import { Overview } from '@/app/_services/overview';
 import PersonaCardSwiper from '@/app/events/[eventId]/overview/_components/persona/PersonaCardSwiper';
 import SurveyActionButton from '@/app/events/[eventId]/overview/_components/SurveyActionButton';
@@ -14,25 +16,32 @@ const OverviewClientPage = ({ overviewData }: { overviewData: Overview }) => {
   const searchParam = useSearchParams();
   const { eventId } = useParams();
   const created = searchParam.get('created');
+  const { isOpen, handler } = useDisclosure();
 
+  console.log('ccccccccccccccc', created);
   useEffect(() => {
     if (created) {
-      router.replace(`/events/${eventId}/overview`);
+      console.log('zxcv');
+      handler.open();
+      // router.replace(`/events/${eventId}/overview`);
     }
   }, []);
 
   return (
-    <div className="flex flex-1 flex-col">
-      <div className="flex flex-col items-center gap-6 px-5 py-4">
-        <SurveyStatusBanner variant="progress" />
-        {/* 하단 모임 참여 상태 공간 차지 */}
-        <div className="h-[75px]" />
-      </div>
+    <>
       <div className="flex flex-1 flex-col">
-        <PersonaCardSwiper overview={overviewData} />
+        <div className="flex flex-col items-center gap-6 px-5 py-4">
+          <SurveyStatusBanner variant="progress" />
+          {/* 하단 모임 참여 상태 공간 차지 */}
+          <div className="h-[75px]" />
+        </div>
+        <div className="flex flex-1 flex-col">
+          <PersonaCardSwiper overview={overviewData} />
+        </div>
+        <SurveyActionButton variant="join" />
       </div>
-      <SurveyActionButton variant="join" />
-    </div>
+      <CreateMeetingSuccessModal isOpen={isOpen} onClose={handler.close} />
+    </>
   );
 };
 
