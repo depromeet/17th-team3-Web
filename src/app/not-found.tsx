67afb879ab1834e5573cabad522795d9c16cd79a
Mark Kwon <redhero8830@gmail.com>
2025-11-08 20:41:15 +0900

    feat: not-found 페이지 구현 (#77)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,32 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
-const NotFound = () => {
+import Button from '@/app/_components/ui/Button';
+
+const NotFoundPage = () => {
   return (
-    <main className="mx-auto max-w-screen-sm p-6 text-center">
-      <h1 className="mb-2 text-2xl font-bold">페이지를 찾을 수 없어요</h1>
-      <p className="mb-6 text-sm text-neutral-500">
-        요청한 페이지가 존재하지 않거나 이동되었을 수 있어요.
-      </p>
-      <Link href="/" className="inline-block rounded-lg px-4 py-2 underline">
-        홈으로 가기
+    <main className="flex h-[100dvh] flex-col">
+      <div className="flex flex-1 flex-col items-center justify-center gap-5">
+        <Image
+          src="/images/momuzzi-not-found.svg"
+          alt="모무찌 404 아이콘"
+          width={180}
+          height={120}
+        />
+        <div className="flex flex-col gap-1 text-center">
+          <h3 className="body-1 font-semibold text-neutral-1600">
+            원하시는 페이지를 찾을 수 없어요
+          </h3>
+          <p className="body-3 whitespace-pre text-neutral-800">
+            {`찾으시는 페이지의 주소 혹은 이름이 변경되었거나,\n일시적으로 사용할 수 없는 상태에요.\n문제가 또 발생한다면, [이메일 주소]로 알려주세요.`}
+          </p>
+        </div>
+      </div>
+      <Link href="/" className="px-5 pt-3 pb-6">
+        <Button>홈으로 가기</Button>
       </Link>
     </main>
   );
 };
 
-export default NotFound;
+export default NotFoundPage;
