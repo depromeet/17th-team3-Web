49490b6aa66d7d9c3bec564232f390771c06309b
jinhyuek <jjh943202@naver.com>
2025-11-14 01:52:13 +0900

    [Feature] MorePicksButton 컴포넌트에 홈으로 버튼 추가 및 레이아웃 수정

--- src/app/events/[eventId]/_components/MorePicksButton.tsx
+++ src/app/events/[eventId]/_components/MorePicksButton.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 import Button from '@/app/_components/ui/Button';
 import { cn } from '@/app/_lib/cn';
 import { ADDITIONAL_RESTAURANT_COUNT } from '@/app/events/[eventId]/_constants/restaurants';
@@ -9,8 +11,15 @@ const MorePicksButton = () => {
   const { increasePickCount, canLoadMore } = useRestaurantPickCount();
 
   return (
-    <div className={cn('sticky bottom-0 px-5 py-3', !canLoadMore && 'hidden')}>
-      <Button onClick={increasePickCount}>{ADDITIONAL_RESTAURANT_COUNT}개 더 추천받기</Button>
+    <div className={cn('sticky bottom-0 flex gap-3 px-5 py-3', !canLoadMore && 'hidden')}>
+      <Link href="/">
+        <Button type="button" theme="gray" className="w-fit px-9 py-5">
+          홈으로
+        </Button>
+      </Link>
+      <Button className="flex-1" onClick={increasePickCount}>
+        {ADDITIONAL_RESTAURANT_COUNT}개 더 추천받기
+      </Button>
     </div>
   );
 };
